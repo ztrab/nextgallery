77fb918da7de773be709db857dab497ecc5ca5f6
Bill Artz <billartzjr@gmail.com>
2024-05-02 12:33:11 -0700

    drizzle db setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-
+import { db } from "@/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/83680858-56d4-43b5-a762-20da3caf3caa-w2vz43.jpg",
@@ -13,12 +13,18 @@ const mockImages = mockUrls.map((url, index) => ({
   url: url
 }));
 
-export default function HomePage() {
+export default async function HomePage() { 
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts)
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((img) => (
-          <div key={img.id} className="w-48">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+        {[...mockImages, ...mockImages, ...mockImages].map((img, index) => (
+          <div key={img.id + "-" + index} className="w-48">
             <img src={img.url} />
           </div>
         ))}
