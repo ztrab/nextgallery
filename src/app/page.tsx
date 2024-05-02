a40593d5963c29a140bf2801e3a47ccfc3a32a41
Bill Artz <billartzjr@gmail.com>
2024-05-02 14:48:10 -0700

    force dynamic and project ts version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { db } from "@/server/db";
 
+export const dynamic = "force-dynamic";
+
 const mockUrls = [
   "https://utfs.io/f/83680858-56d4-43b5-a762-20da3caf3caa-w2vz43.jpg",
   "https://utfs.io/f/83680858-56d4-43b5-a762-20da3caf3caa-w2vz43.jpg",
@@ -13,7 +15,7 @@ const mockImages = mockUrls.map((url, index) => ({
   url: url
 }));
 
-export default async function HomePage() { 
+export default async function HomePage() {
   const posts = await db.query.posts.findMany();
 
   console.log(posts)
