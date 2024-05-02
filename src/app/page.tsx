8e26f044e6b610e358bf5ec40866287019737ce6
Bill Artz <billartzjr@gmail.com>
2024-05-02 15:27:30 -0700

    linking db to ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,31 +3,33 @@ import { db } from "@/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/83680858-56d4-43b5-a762-20da3caf3caa-w2vz43.jpg",
-  "https://utfs.io/f/83680858-56d4-43b5-a762-20da3caf3caa-w2vz43.jpg",
-  "https://utfs.io/f/83680858-56d4-43b5-a762-20da3caf3caa-w2vz43.jpg",
-  "https://utfs.io/f/83680858-56d4-43b5-a762-20da3caf3caa-w2vz43.jpg",
-];
+// const mockUrls = [
+//   "https://utfs.io/f/83680858-56d4-43b5-a762-20da3caf3caa-w2vz43.jpg",
+//   "https://utfs.io/f/83680858-56d4-43b5-a762-20da3caf3caa-w2vz43.jpg",
+//   "https://utfs.io/f/83680858-56d4-43b5-a762-20da3caf3caa-w2vz43.jpg",
+//   "https://utfs.io/f/83680858-56d4-43b5-a762-20da3caf3caa-w2vz43.jpg",
+// ];
 
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url: url
-}));
+// const mockImages = mockUrls.map((url, index) => ({
+//   id: index + 1,
+//   url: url
+// }));
 
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id) 
+  });
 
-  console.log(posts)
+  console.log(images)
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-        {[...mockImages, ...mockImages, ...mockImages].map((img, index) => (
-          <div key={img.id + "-" + index} className="w-48">
-            <img src={img.url} />
+        {[...images, ...images, ...images].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48 flex flex-col">
+            <img src={image.url} alt={image.name} />
+            <div className="">
+              {image.name}
+            </div>
           </div>
         ))}
       </div>
