4efd05c20bbeeea4059ac728c969d50912e1a29f
Bill Artz <billartzjr@gmail.com>
2024-05-06 16:09:40 -0700

    grid fix, modal tweaks and remove repeat data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
           routerConfig={extractRouterConfig(ourFileRouter)}
         />
         <body className={`font-sans ${inter.variable}`}>
-          <div className="grid-rows-[auto, 1fr] grid h-screen">
+          <div className="grid h-screen grid-rows-[auto,1fr]">
             <TopNav />
             <main className="overflow-y-scroll">{children}</main>
           </div>
--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,15 +1,28 @@
 import { getImage } from "@/server/queries";
+import { clerkClient } from "@clerk/nextjs/server";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
+  const uploaderInfo = await clerkClient.users.getUser(image.userId);
+
   return (
-    <div className="min-w-0a flex h-full w-full">
+    <div className="flex h-full w-screen min-w-0">
       <div className="flex flex-shrink items-center justify-center">
-        <img src={image.url} className="object-contain" />
+        <img src={image.url} className="object-contain" alt={image.name} />
       </div>
 
-      <div className="flex w-48 flex-shrink-0 flex-col border-l">
-        <div className="text-xl font-bold">{image.name}</div>
+      <div className="flex w-48 flex-shrink-0 flex-col gap-2 border-l">
+        <div className="border-b p-2 text-center text-lg">{image.name}</div>
+
+        <div className="flex flex-col p-2">
+          <span>Uploaded by: </span>
+          <span> {uploaderInfo.fullName} </span>
+        </div>
+
+        <div className="flex flex-col p-2">
+          <span>Created on: </span>
+          <span> {new Date(image.createdAt).toLocaleDateString()} </span>
+        </div>
       </div>
     </div>
   );
