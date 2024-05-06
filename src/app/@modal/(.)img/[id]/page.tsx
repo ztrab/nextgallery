76d4846f2acd60332dfa0f76713f9c26d48b41c1
Bill Artz <billartzjr@gmail.com>
2024-05-06 13:02:09 -0700

    modal dialog fix for fullscreen and component for image view

--- src/app/@modal/(.)img/[id]/page.tsx
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -1,6 +1,7 @@
-import { getImage } from "@/server/queries";
+import { Modal } from "./modal";
+import FullPageImageView from "@/components/full-image-page";
 
-export default async function PhotoModal({
+export default function PhotoModal({
   params: { id: photoId },
 }: {
   params: { id: string };
@@ -8,10 +9,9 @@ export default async function PhotoModal({
   const idAsNum = Number(photoId);
   if (Number.isNaN(idAsNum)) throw new Error("Invalid photo id");
 
-  const image = await getImage(idAsNum);
   return (
-    <div>
-      <img src={image.url} className="w-96" />
-    </div>
+    <Modal>
+      <FullPageImageView id={idAsNum} />
+    </Modal>
   );
 }
